044b6ede8a834b7825d53b7ffea70b1b5600c3a2
Nick Robison <nicholas.a.robison@omb.eop.gov>
2021-02-18 10:27:09 -0500

    Test varint pack/unpack

--- test/unity/unit_tests.c
+++ test/unity/unit_tests.c
@@ -1,7 +1,8 @@
 #include "unity_fixture.h"
 
 static void RunAllTests(void) {
-    RUN_TEST_GROUP(SerializationTests)
+    RUN_TEST_GROUP(SerializationTests);
+    RUN_TEST_GROUP(VarintTests);
 }
 
 int main(int argc, const char * argv[]) {
